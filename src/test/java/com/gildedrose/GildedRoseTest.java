0f6fb92c2c460dbc68713ccb892209b82d5a95fc
Elie NEHME <elie.nehme@soprasterianext.com>
2022-06-21 15:17:07 +0200

    Couverture et test solide à 97%

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,31 @@
 package com.gildedrose;
 
+import org.approvaltests.combinations.CombinationApprovals;
+import org.approvaltests.reporters.ClipboardReporter;
+import org.approvaltests.reporters.UseReporter;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Arrays;
 
+@UseReporter(ClipboardReporter.class)
 class GildedRoseTest {
 
    @Test
    void testUpdateQuality() {
-      Item[] items = new Item[] { new Item("foo", 0, 2) };
+      CombinationApprovals.verifyAllCombinations(
+         this::doUpdate,
+         new String[] { "foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros" },
+         new Integer[] { -1, 0, 5, 6, 7, 10, 11, 12 },
+         new Integer[] { -1, 0, 1, 2, 48, 49, 50, 51 }
+      );
+   }
+
+   private String doUpdate(String name, int sellIn, int quality) {
+      Item[] items = new Item[] { new Item(name, sellIn, quality) };
       GildedRose app = new GildedRose(items);
       app.updateQuality();
-      assertThat(items[0].name).isEqualTo("foo");
+
+      return Arrays.toString(items);
    }
 
 }
